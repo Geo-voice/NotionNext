ffcfcadabf362cab7b375d5b69ac9b5235709a3b
Geo-voice <xinshun.okinawa@gmail.com>
2024-09-03 13:31:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Unique Voice STU', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Perfessional Chinese voice actors🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.youtube.com/@WarmVoice1978', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, dubbing, voice actor', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'dubbing, voice actor', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'xinshun.okinawa@gmail.com', // 邮箱地址 例如mail@tangly1024.com
